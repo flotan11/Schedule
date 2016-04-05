e7c62061c1299279b59ec377bf0f832d0470fbe9
flotan11 <mardon.florian@gmail.com>
2016-04-05 14:41:30 +0200

    Index Skeleton.

---
+++ index.php
@@ -0,0 +1,16 @@
+<html>
+<header>
+	<title>Meeting Plannifier</title>
+</header>
+<body>
+<div name="presentation">
+<h1><strong>Meeting Planifier</strong></h1>
+<p>Please enter your Student ID to access teacher schedule :</p>
+</div>
+<form method="POST" action="log.php">
+	<p>INE : <input type="text" name="INE"/></p>
+	<p>Teacher : <input type="text" name="INP"/></p>
+	<button>Validate</button>
+</form>
+</body>
+</html>
