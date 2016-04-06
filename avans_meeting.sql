9407d7cc397882f2048a2de0a36cee870e141279
flotan11 <mardon.florian@gmail.com>
2016-04-06 09:17:27 +0200

    SQL model.

---
+++ avans_meeting.sql
@@ -0,0 +1,108 @@
+-- phpMyAdmin SQL Dump
+-- version 4.5.1
+-- http://www.phpmyadmin.net
+--
+-- Client :  127.0.0.1
+-- Généré le :  Mer 06 Avril 2016 à 09:15
+-- Version du serveur :  10.1.10-MariaDB
+-- Version de PHP :  7.0.4
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de données :  `avans_meeting`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `event`
+--
+
+CREATE TABLE `event` (
+  `event-title` int(7) NOT NULL,
+  `teacher-id` int(7) NOT NULL,
+  `startdate` date NOT NULL,
+  `endate` date NOT NULL,
+  `location` varchar(20) NOT NULL,
+  `remarks` varchar(30) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `student`
+--
+
+CREATE TABLE `student` (
+  `student-id` int(7) NOT NULL,
+  `logincode` varchar(20) NOT NULL,
+  `firstname` varchar(20) NOT NULL,
+  `prefix` varchar(10) DEFAULT NULL,
+  `lastname` varchar(20) NOT NULL,
+  `group` varchar(10) NOT NULL,
+  `email` varchar(30) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `subscription`
+--
+
+CREATE TABLE `subscription` (
+  `student-id` int(7) NOT NULL,
+  `event-title` int(7) NOT NULL,
+  `date` date NOT NULL,
+  `time` time NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `teacher`
+--
+
+CREATE TABLE `teacher` (
+  `teacher-id` int(7) NOT NULL,
+  `logincode` varchar(20) NOT NULL,
+  `firstname` varchar(20) NOT NULL,
+  `prefix` varchar(10) DEFAULT NULL,
+  `lastname` varchar(20) DEFAULT NULL,
+  `email` varchar(30) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Index pour les tables exportées
+--
+
+--
+-- Index pour la table `event`
+--
+ALTER TABLE `event`
+  ADD PRIMARY KEY (`event-title`);
+
+--
+-- Index pour la table `student`
+--
+ALTER TABLE `student`
+  ADD PRIMARY KEY (`student-id`),
+  ADD UNIQUE KEY `ine` (`student-id`);
+
+--
+-- Index pour la table `teacher`
+--
+ALTER TABLE `teacher`
+  ADD PRIMARY KEY (`teacher-id`),
+  ADD UNIQUE KEY `inp` (`teacher-id`);
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
--- index.php
+++ index.php
@@ -3,14 +3,19 @@
 <link rel="stylesheet" href="css/style.css" type="text/css"/>
 </head>
 <header>
-
 	<title>Meeting Plannifier</title>
 </header>
 <body>
-<div id="presentation">
-<h1><strong>Meeting Planifier</strong></h1>
-<p>Please enter your Student ID to access teacher schedule :</p>
-</div>
+<table>
+<tr><td>
+	<img id="icon" src="img/avans.png" alt="AVANS Icon">
+	</td><td>
+	<div id="presentation">
+	<h1><strong>Meeting Planifier</strong></h1>
+	<p>Please enter your Student ID to access teacher schedule :</p>
+	</div>
+</td></tr>
+</table>
 <form method="POST" action="log.php">
 	<p>INE : <input type="text" name="INE"/></p>
 	<p>Teacher : <input type="text" name="INP"/></p>
