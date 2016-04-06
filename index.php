3bb7539442e7a9b65b34083f7095c6516719a2b0
flotan11 <mardon.florian@gmail.com>
2016-04-06 08:35:19 +0200

    Upgrade design index.php

--- index.php
+++ index.php
@@ -1,16 +1,20 @@
 <html>
+<head>
+<link rel="stylesheet" href="css/style.css" type="text/css"/>
+</head>
 <header>
+
 	<title>Meeting Plannifier</title>
 </header>
 <body>
-<div name="presentation">
+<div id="presentation">
 <h1><strong>Meeting Planifier</strong></h1>
 <p>Please enter your Student ID to access teacher schedule :</p>
 </div>
 <form method="POST" action="log.php">
 	<p>INE : <input type="text" name="INE"/></p>
 	<p>Teacher : <input type="text" name="INP"/></p>
-	<button>Validate</button>
 </form>
+<button>Validate</button>
 </body>
 </html>
