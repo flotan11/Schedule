8cd3de1ab08ce3ffcfee5f195f78d7baf55f1cbc
flotan11 <mardon.florian@gmail.com>
2016-04-07 15:29:18 +0200

    Upgrade menu.php (We can naviguate in past and futur weeks now).

--- menu.php
+++ menu.php
@@ -6,28 +6,17 @@
 	<title>Meeting Plannifier</title>
 </header>
 <body>
-<table>
-<tr><td>
-	<img id="icon" src="img/avans.png" alt="AVANS Icon">
-</td><td>
-	<div id="presentation">
-		<h1><strong>Meeting Planifier</strong></h1>
-		<p>Welcome to the schedule of Mr.<?php  echo $_COOKIE['teacher_name'] ?></p>
-		<p>Please select the date you would like to meet him :</p>
-	</div>
-</td></tr>
-</table>	
 
-<table id='schedule'>
-<tr>
-	<?php
-		$dayw=date('w')-1;
-		$day=date('l d F');
+<?php 
+	function move($v){
+		$dayw=date('w')-1+($v*7);
+		echo '<table id=\'schedule\'><tr>';
+		$day=date('l d F Y');
 		$day=new DateTime($day);
 		$day->modify('-'.$dayw.' day');
 		$monday=$day;
 		for ($i=0;$i<5;$i++){
-			echo '<td class=\'name\'><strong>'.date_format($day,'l d F').'</strong></td>';
+			echo '<td class=\'name\'><strong>'.date_format($day,'l d F Y').'</strong></td>';
 			$day->modify('+1 day');
 		}
 		echo '</tr><tr>';
@@ -48,14 +37,39 @@
 			$h++;
 			echo '</tr>';
 		}
+		echo '</table>';
+	}
+?>
+
+<table>
+<tr><td>
+	<img id="icon" src="img/avans.png" alt="AVANS Icon">
+</td><td>
+	<div id="presentation">
+		<h1><strong>Meeting Planifier</strong></h1>
+		<p>Welcome to the schedule of Mr.<?php  echo $_COOKIE['teacher_name'] ?></p>
+		<p>Please select the date you would like to meet him :</p>
+	</div>
+</td></tr>
+</table>	
+	<?php
+		if(isset($_POST['button'])){
+			$f=$_POST['button'];
+		}else{
+			$f=0;
+		}
+		move($f);
 	?>
-</table>
+<form method='POST' action='menu.php'>
 <table id='move'>
 <tr><td>
-	<button id='previous'>Previous</button>
+	<button id='previous' value='<?php echo $f+1; ?>' name='button'>Previous</button>
 </td><td>
-	<button id='next'>Next</button>
+	<button id='actual' value='0' name='button'>Actual</button>
+</td><td>
+	<button id='next' value='<?php echo $f-1; ?>' name='button'>Next</button>
 </td></tr>
 </table>
+</form>
 </body>
 </html>
