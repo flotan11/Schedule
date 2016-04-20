d140f51ddf4235db961505cba12a09c8f0bb8a92
flotan11 <mardon.florian@gmail.com>
2016-04-20 14:26:48 +0200

    css miss filepath fixed

--- viewer/root/menu_root.php
+++ viewer/root/menu_root.php
@@ -4,7 +4,7 @@ session_start ();
 <html>
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<link rel="stylesheet" href="css/style_root.css" type="text/css" />
+<link rel="stylesheet" href="../../css/style_root.css" type="text/css" />
 </head>
 <header>
 	<title>Meeting Plannifier</title>
