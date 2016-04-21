0cd91bea01a260eba4baad72985b674cf26a89a4
AlexandreDupriez <alexdup59@hotmail.fr>
2016-04-21 15:24:40 +0200

    insert root modification

--- controller/checkInsert_root.php
+++ controller/checkInsert_root.php
@@ -1,3 +1,7 @@
+<?php 
+	session_start();
+?>
+
 <?php
 $servername = "home.spijkerman.nl";
 $username = "subs";
@@ -8,28 +12,47 @@ try {
 	$conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
 	// set the PDO error mode to exception
 	$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-
+	
+	
+	if($_SESSION['table']== "student"){
 	$sql1 = "INSERT INTO student (student_id,login_code,firstname, prefix,lastname,group_name ,email, team_name)
-    VALUES (".$_POST['student_id'].",".$_POST['login_code'].", '".$_POST['firstname']."' , '".$_POST['prefix']."' , '".$_POST['lastname']."' , '".$_POST['group_name']."' , '".$_POST['email']."' , '".$_POST['team_name']."')";
-
-	$sql2 = "INSERT INTO teacher (teacher_id,logincode,firstname, prefix,lastname,email)
-    VALUES (".$_POST['teacher_id'].",".$_POST['logincode'].", '".$_POST['firstname']."' , '".$_POST['prefix']."' , '".$_POST['lastname']."' , '".$_POST['email']."')";
-
-	$sql3 = "INSERT INTO subscription (event_name,student_id,date, time)
-    VALUES ('".$_POST['event_name']."',".$_POST['student_id'].", '".$_POST['date']."' , '".$_POST['time']."')";   
-
-	/* $sql = "INSERT INTO event (event_name,teacher_id,startdate,endate,location,remarks)
-	 VALUES ('".$_POST['event_name']."',".$_POST['teacher_id'].",'".$_POST['startdate']."' , '".$_POST['endate']."','".$_POST['location']."','".$_POST['remarks']."')"*/
-	// use exec() because no results are returned
-	$conn->exec($sql2);
+    VALUES (".$_POST['student_idstudent'].",".$_POST['login_codestudent'].", '".$_POST['firstnamestudent']."' , '".$_POST['prefixstudent']."' , '".$_POST['lastnamestudent']."' , '".$_POST['group_namestudent']."' , '".$_POST['emailstudent']."' , '".$_POST['team_namestudent']."')";
 	$conn->exec($sql1);
-	$conn->exec($sql3);
+	}
+	 
+	if($_SESSION['table']=="teacher"){
+	$sql2 = "INSERT INTO teacher (teacher_id,logincode,firstname, prefix,lastname,email)
+    VALUES (".$_POST['teacher_idteacher'].",".$_POST['logincodeteacher'].", '".$_POST['firstnameteacher']."' , '".$_POST['prefixteacher']."' , '".$_POST['lastnameteacher']."' , '".$_POST['emailteacher']."')";
+	$conn->exec($sql2); 
+	}
+	
+	if($_SESSION['table']=="subscription"){
+	$sql3 = "INSERT INTO subscription (event_name,student_id,date, time)
+    VALUES ('".$_POST['event_namesubscription']."',".$_POST['student_idsubscription'].", '".$_POST['datesubscription']."' , '".$_POST['timesubscription']."')";   
+	$conn->exec($sql3); 
+	}
+	
+	if($_SESSION['table']=="event"){
+	 $sql4 = "INSERT INTO event (event_name,teacher_id,startdate,endate,location,remarks)
+	 VALUES ('".$_POST['event_nameevent']."',".$_POST['teacher_idevent'].",'".$_POST['startdateevent']."' , '".$_POST['endateevent']."','".$_POST['locationevent']."','".$_POST['remarksevent']."')";
+	$conn->exec($sql4); 
+	}
+
+	 
+	 // use exec() because no results are returned
+
+	
+//	$conn->exec($sql3);
 	header('Location: ../viewer/root/menu_root.php');
+	
+	
 }
 catch(PDOException $e)
 {
-	header('Location: ../viewer/root/menu_root.php');
+	echo $sql3 . "<br>" . $e->getMessage();
+//	header('Location: ../viewer/root/menu_root.php');
 }
 
 $conn = null;
+
 ?>
--- viewer/root/insert_root.php
+++ viewer/root/insert_root.php
@@ -51,25 +51,28 @@ if (! isset ( $_SESSION ['logged'] ) || $_SESSION ['status'] != 'root') {
 	<input type="submit" value="Table Teacher" name="teac" style="background-color: #c6002a; border: 2px solid black;  width: 120px; height: 40px;" />
 	<input type="submit" value="Table Event" name="even" style="background-color: #c6002a; border: 2px solid black;  width: 120px; height: 40px;" />
 	<input type="submit" value="Table Student" name="stud" style="background-color: #c6002a; border: 2px solid black;  width: 120px; height: 40px;"> 
+</form>
 
 <?php 
- if (isset($_POST['subs'])){
+ if (isset($_POST['stud'])){
 
- 	
+$_SESSION['table'] = 'student'; 	
 ?>
 
 <table>
+   <div id="formulaire">
 	<form method="POST" action="../../controller/checkInsert_root.php">
 		<p>student_id : <input type="text" name="student_idstudent"/>
-		logincode : <input type="text" name="login_codestudent"/>
-		firstname : <input type="text" name="firstnamestudent"/>
-		prefix : <input type="text" name="prefixstudent"/>
-		lastname : <input type="text" name="lastnamestudent"/>
-		group_name : <input type="text" name="group_namestudent"/>
-		email : <input type="text" name="emailstudent"/>
-		Team  : <input type="text" name="team_namestudent"/>
+		<p>logincode : <input type="text" name="login_codestudent"/>
+		<p>firstname : <input type="text" name="firstnamestudent"/>
+		<p>prefix : <input type="text" name="prefixstudent"/>
+		<p>lastname : <input type="text" name="lastnamestudent"/>
+		<p>group_name : <input type="text" name="group_namestudent"/>
+		<p>email : <input type="text" name="emailstudent"/>
+		<p>Team  : <input type="text" name="team_namestudent"/>
 		<input type="Submit"  value="Enter"></p>
 	</form>
+	</div>
 	</table>
 
     
@@ -78,19 +81,21 @@ if (! isset ( $_SESSION ['logged'] ) || $_SESSION ['status'] != 'root') {
       } 
       if(isset($_POST['teac'])){
 
- 	
+$_SESSION['table'] = 'teacher'; 	
 ?>
 
 <table>
+   <div id="formulaire">
 	<form method="POST" action="../../controller/checkInsert_root.php">
 		<p>teacher_id : <input type="text" name="teacher_idteacher"/>
-		logincode : <input type="text" name="logincodeteacher"/>
-		firstname : <input type="text" name="firstnameteacher"/>
-		prefix : <input type="text" name="prefixteacher"/>
-		lastname : <input type="text" name="lastnameteacher"/>
-		email : <input type="text" name="emailteacher"/>
+		<p>logincode : <input type="text" name="logincodeteacher"/>
+		<p>firstname : <input type="text" name="firstnameteacher"/>
+		<p>prefix : <input type="text" name="prefixteacher"/>
+		<p>lastname : <input type="text" name="lastnameteacher"/>
+		<p>email : <input type="text" name="emailteacher"/>
 		<input type="Submit"  value="Enter"></p>
 	</form>
+	</div>
 	</table>
 
     
@@ -99,18 +104,20 @@ if (! isset ( $_SESSION ['logged'] ) || $_SESSION ['status'] != 'root') {
       } 
       
       if(isset($_POST['even'])){
-
+$_SESSION['table'] = 'event'; 	
  	
 ?>
 
 <table>
+   <div id="formulaire">
 	<form method="POST" action="../../controller/checkInsert_root.php">
 		<p>event_name : <input type="text" name="event_namesubscription"/>
-		student_id : <input type="text" name="student_idsubscription"/>
-		date : <input type="date" name="datesubscription"/>
-		time : <input type="time" name="timesubscription"/>
+		<p>student_id : <input type="text" name="student_idsubscription"/>
+		<p>date : <input type="date" name="datesubscription"/>
+		<p>time : <input type="time" name="timesubscription"/>
 		<input type="Submit"  value="Enter"></p>
 	</form>
+	</div>
 	</table>
 
     
@@ -118,23 +125,24 @@ if (! isset ( $_SESSION ['logged'] ) || $_SESSION ['status'] != 'root') {
     
       } 
       
-       if(isset($_POST['stud'])){
-       		
+       if(isset($_POST['subs'])){
+      $_SESSION['table'] = 'subscription'; 	 		
 ?>
 
 <table>
+   <div id="formulaire">
 	<form method="POST" action="../../controller/checkInsert_root.php">
 		<p>event_name : <input type="text" name="event_nameevent"/>
-		teacher_id : <input type="text" name="teacher_idevent"/>
-		startdate : <input type="date" name="startdateevent"/>
-		endate : <input type="date" name="endateevent"/>
-		location : <input type="text" name="locationevent"/>
-		remarks : <input type="text" name="remarksevent"/>
+		<p>teacher_id : <input type="text" name="teacher_idevent"/>
+		<p>startdate : <input type="date" name="startdateevent"/>
+		<p>endate : <input type="date" name="endateevent"/>
+		<p>location : <input type="text" name="locationevent"/>
+		<p>remarks : <input type="text" name="remarksevent"/>
 		
 		<input type="Submit"  value="Enter"></p>
 	</form>
+	</div>
 	</table>
-
     
     <?php 
     
