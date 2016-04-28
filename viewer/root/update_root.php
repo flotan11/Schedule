f8d4f971d6acec1978afa20673576a25099009ca
AlexandreDupriez <alexdup59@hotmail.fr>
2016-04-28 09:08:43 +0200

    editing update root

--- viewer/root/update_root.php
+++ viewer/root/update_root.php
@@ -65,40 +65,61 @@ try {
 	$conn->setAttribute ( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
 	$stmt = $conn->prepare ( "SELECT * FROM rien" );
 
-	
+		echo"<div id = 'global'";
 		echo "<div id='tables'>";
 			if(isset ( $_POST ['subs'] )) {
 			echo "<font face='purisa' color='#00ACEB' size='6px'><p align='left'>Subscription Table </p></font> ";
 			echo "<br><table style='border: solid 3px black;background-color : #00ACEB;'>";
 			echo "<tr><th>event_name</th><th>student_id</th><th>date</th><th>time</th><th>Modification</th></tr>";
 			$stmt = $conn->prepare ( "SELECT * FROM subscription" );
+			$res = $conn->query('select count(*) as nb from subscription');
 		} elseif (isset ( $_POST ['stud'] )) {
 			echo "<font face='purisa' color='#00ACEB' size='6px'><p align='left'>Student Table </p></font> ";
 			echo "<br><table style='border: solid 3px black;background-color : #00ACEB;'>";
 			echo "<tr><th>student_id</th><th>login_code</th><th>firstname</th><th>prefix</th><th>lastname</th><th>group</th><th>mail</th><th>team_name</th><th>Modification</th></tr>";
 			$stmt = $conn->prepare ( "SELECT * FROM student" );
+		    $res = $conn->query('select count(*) as nb from student');
 		} elseif (isset ( $_POST ['even'] )) {
 			echo "<font face='purisa' color='#00ACEB' size='6px'><p align='left'>Event Table </p></font> ";
 			echo "<br><table style='border: solid 3px black;background-color : #00ACEB;'>";
 			echo "<tr><th>event_name</th><th>teacher_id</th><th>startdate</th><th>endate</th><th>location</th><th>remarks</th><th>Modification</th></tr>";
 			$stmt = $conn->prepare ( "SELECT * FROM event" );
+		    $res = $conn->query('select count(*) as nb from event');
 		} elseif (isset ( $_POST ['teac'] )) {
 			echo "<font face='purisa' color='#00ACEB' size='6px'><p align='left'>Teacher Table </p></font> ";
 			echo "<br><table style='border: solid 3px black;background-color : #00ACEB;'>";
 			echo "<tr><th>teacher_id</th><th>logincode</th><th>firstname</th><th>prefix</th><th>lastname</th><th>mail</th><th>Modification</th></tr>";
 			$stmt = $conn->prepare ( "SELECT * FROM teacher" );
+			$res = $conn->query('select count(*) as nb from teacher');
 		} else {
 			$stmt = $conn->prepare ( "SELECT * FROM rien" );
 		}
-	
+		
+    $data = $res->fetch();
+    $nb = $data['nb'];
+    echo $nb;
+
 	
 	$stmt->execute ();
-	
+	$cpt=0;
 	// set the resulting array to associative
 	$result = $stmt->setFetchMode ( PDO::FETCH_ASSOC );
 	foreach ( new TableRows ( new RecursiveArrayIterator ( $stmt->fetchAll () ) ) as $k => $v ) {
 		echo $v;
+
 	}
+
+	
+	echo "</div>";
+	
+	echo "<div id = 'mod_button'>";
+	for($i=0; $i<$nb; $i++){
+		
+			echo "<input type='submit' value='mod' name='stud' style='background-color: #c6002a; border: 2px solid black;  width: 120px; height: 40px;'> 
+		";
+			
+	}
+	echo "</div>";
 	echo "</div>";
 } 
 
